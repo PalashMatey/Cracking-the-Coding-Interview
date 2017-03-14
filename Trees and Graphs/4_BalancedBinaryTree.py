c311a042904f7fd565c78096d1f8c6a5bbf6600f
PalashMatey <pm2824@columbia.edu>
2017-03-13 23:38:02 -0400

    Balanced Tree

---
+++ Trees and Graphs/4_BalancedBinaryTree.py
@@ -0,0 +1,14 @@
+def balanced(self, root):
+
+    def check(root):
+        while not root:
+            return 0
+        left = check(root.left)
+        right = check(root.right)
+
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return 1 + max(left,right)
+        
+    return check(root) != -1
+
